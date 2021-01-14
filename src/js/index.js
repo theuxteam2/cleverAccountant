9f894c0c711e6a2e34fac27d90b8606bbf57ac90
Loi Pham <loivp13@gmail.com>
2021-01-13 22:01:50 -0800

    finished contactUs page

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,55 @@
 (function(){
+    //MOVE TITLE PRICING
    document.addEventListener("DOMContentLoaded", function(event) {
+    
+       var card = document.querySelector('.Service-cards')
+       if(card){
+       card.addEventListener('scroll', check);
+       }
+   
+        //Contact page validation
+       var name = document.querySelector('#Name')
+       if(name){
+        name.addEventListener('invalid', validate);
        
-       let cards = document.querySelector('.Service-cards');
-      
-       let title = document.querySelector('.Service-titles');
-       
-       cards.addEventListener('scroll', check)
-        
+
+       }
+       var email = document.querySelector('#Email')
+       if(email){
+         email.addEventListener('invalid', validate);
+
+
+       }
+
+       var closeButton = document.querySelector('.Contact-modal-close');
+       if(closeButton){
+           closeButton.addEventListener('click', toggleModal)
+       }
+
+       var form = document.querySelector('#Contact-form')
+       if(form){
+           form.addEventListener('submit', function(e){
+               e.preventDefault();
+              Email.send({
+                    SecureToken : "798b09de-95ac-4f38-9cec-6f4bb9f2bc33",
+                    To : 'info@thecleveraccountants.com',
+                    From : "".concat(document.querySelector('#Email').value),
+                    Subject : "".concat(document.querySelector('#packages').value),
+                    Body : "\n                    <div className=\"\">\n                        Name: ".concat(document.querySelector('#Name').value, " <br/>\n                        Email: ").concat(document.querySelector('#Email').value, " <br/>\n                        Business name : ").concat(document.querySelector('#Business').value, "  <br/>\n                        Industry : ").concat(document.querySelector('#Industry').value, " <br/>\n                        Phone Number : ").concat(document.querySelector('#phone').value, " <br/>\n                        Preferred Method of Communication : ").concat(document.querySelector('#communication').value, "\n                        <br/> <br/>\n                        ").concat(document.querySelector('#Message').value, "\n\n                    </div>\n                    ")
+                }).then(
+               toggleModal()
+                );
+           })
+       }
 
     });
 
-    let check = debounce(checkScroll(),200)
+    var check = debounce(checkScroll(),100)
 
+    var title;
     function checkScroll (){
-        let current = 0;
-         let title = document.querySelector('.Service-titles');
+        var current = 0;
+        title = document.querySelector('.Service-titles');
         
         return function(){
         if(this.scrollTop <= 32){
@@ -56,15 +91,37 @@
         }
         
     }
+
+    //UTILITY FUNCTION
+    function sendEmail(){
+
+    }
+   
+    function validate(e){
+        e.preventDefault();
+        
+        document.querySelector("label[for=".concat((void 0).id, "]")).style.color= 'red'
+
+        document.querySelector('.Contact-form-validation').style.display ='block';
+        
+    }
+    function toggleModal(){
+        document.querySelector('.Contact-modal').classList.toggle('Contact-modal-display');
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
     function debounce(func, wait, immediate) {
-	    let timeout;
+	    var timeout;
         return function() {
-            let context = this, args = arguments;
-            let later = function() {
+            var context = this, args = arguments;
+            var later = function() {
                 timeout = null;
                 if (!immediate) func.apply(context, args);
             };
-            let callNow = immediate && !timeout;
+            var callNow = immediate && !timeout;
             clearTimeout(timeout);
             timeout = setTimeout(later, wait);
             if (callNow) func.apply(context, args);
